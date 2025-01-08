1e27dc146cbaa122073bf65663edbd23230e2903
virgilijusjavtokas <virgilijus.javtokas@gmail.com>
2025-01-08 15:14:25 +0200

    Kodas veikia, viskas sukelta i faila main.py. dar reikia patikrinti klaidas

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Pradžioje planavau naudoti daugiau failu, tad lenteles atvaizdavimo funkciją buvau padaręs atskiramefaile, bet
+# nusprendžiau viską sudėti į vieną failą, todėl liko failas lentele.py nepanaudotas ir sekanti import komanda
+# užkomentuota.
+
 # from lentele import atvaizduoti_lentele
 def atvaizduoti_lentele(langeliai):
     lentele = (f" {langeliai[1]} | {langeliai[2]} | {langeliai[3]} \n-----------\n"
@@ -5,13 +9,13 @@ def atvaizduoti_lentele(langeliai):
                f"{langeliai[7]} | {langeliai[8]} | {langeliai[9]} ")
     print(lentele)
 
-def kas_eina(ejimas):
+def kas_eina(ejimas): #tikrina kieno ejimas
     if ejimas % 2 == 0:
         return "0"
     else:
         return "X"
 
-def tikrinimas(dict):
+def tikrinimas(dict): #tikrina ar laimejo
     #tikrina eilutes
     if (langeliai[1] == langeliai[2] == langeliai[3] or langeliai[4] == langeliai[5] == langeliai[6] or \
             langeliai[7] == langeliai[8] == langeliai[9]):
@@ -25,24 +29,25 @@ def tikrinimas(dict):
         return True
     else: return False
 
-zaidimas = True
-ejimas = 1
+
 langeliai = {1 : "1", 2 : "2", 3 : "3", 4 : "4", 5 : "5", 6 : "6", 7 : "7", 8 : "8", 9 : "9"}
+ejimas = 1
+ejimai = []
+x_pergales = 0
+o_pergales = 0
 
 print("\nNULIUKŲ IR KRYŽIUKŲ ŽAIDIMAS")
 print("----------------------------")
 print("""Taisyklės: 
 Pirmas žaidėjas žaidžia 'X', o antras '0'. 
 Žaidėjas įveda skaičių, kuris žymi vietą lentelėje.
-Jei norite pabaigti žaidimą - iveskite 'N'
+Jei norite bet kada pabaigti žaidimą - iveskite 'B'
 PRADEDAM !\n""")
+
 atvaizduoti_lentele(langeliai)
-ejimai = []
-x_pergales = 0
-o_pergales = 0
 
 while True:
-    pasirinkimas = input("Pasirinkite vieta ivesdami ja zyminti skaiciu (veskite 'B' jei norite iseiti is zaidimo): ")
+    pasirinkimas = input("Pasirinkite vietą įvesdami ją žymintį skaičių (veskite 'B' jei norite išeiti iš žaidimo): ")
     if pasirinkimas == "B": break
     else:
         if pasirinkimas.isdigit():
@@ -56,10 +61,18 @@ while True:
                     print(f"Laimejo {langeliai[int(pasirinkimas)]} zaidejas")
                     if langeliai[int(pasirinkimas)] == "X": x_pergales += 1
                     elif langeliai[int(pasirinkimas)] == "0": o_pergales += 1
-                    break
+                    # zaidimas = False
+                    arzaidziam = input("Ar norite zaisti dar karta? (T/N): ")
+                    if arzaidziam == "T":
+                        langeliai = {1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9"}
+                        atvaizduoti_lentele(langeliai)
+                        ejimas = 1
+                        ejimai = []
+                    else:
+                        print(f"Žaidimas baigtas. 'X' žaidėjo pergalės: {x_pergales}, 'O' žaidėjo pergalės: {o_pergales}")
+                        break
             else:
-                print("Ivedete neteisinga ejima - bandykite dar karta.")
+                print("Įvedėte neteisingą ėjimą - bandykite dar kartą.")
         else:
-            print("Ivedete neteisinga ejima - bandykite dar karta.")
-
+            print("Įvedėte neteisingą ėjimą - bandykite dar kartą")
 #
